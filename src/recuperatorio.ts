ae41c2befb8e71aad53e4fb681dd39ce27f1e775
Federico Batalla <104173891+federico-batalla@users.noreply.github.com>
2022-06-19 04:15:02 -0300

    carga de productos

--- src/recuperatorio.ts
+++ src/recuperatorio.ts
@@ -3,15 +3,21 @@ function cargarProductos(
   productos: string[],
   precios: number[],
   cantidades: number[]
-): void {}
+): void {
+  for (let i: number = 0; i < productos.length; i++) {
+    productos[i] = prompt("ingrese producto");
+    precios[i] = Number(prompt("ingrese precio del producto "));
+    cantidades[i] = Number(prompt("ingrese cantidad a llevar del producto"));
+  }
+}
 
 //-------------------------------------------------
 
 let cantidad: number = Number(
   prompt("ingrese catidad de productos que va a comprar")
 );
-let productos: string[cantidad];
-let precios: number[cantidad];
-let cantidades: number[cantidad];
+let productos: string[] = Array(cantidad);
+let precios: number[] = Array(cantidad);
+let cantidades: number[] = Array(cantidad);
 
 cargarProductos(productos, precios, cantidades);
